38c56ff2467182956d037a859a35a685f638b792
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-09-23 08:39:00 +0300

    code

--- src/BonusService.java
+++ src/BonusService.java
@@ -1,3 +1,11 @@
 public class BonusService {
-
+    public long calculate(final long amount, final boolean registered) {
+        final int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100 / 100;
+        final long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        return bonus;
+    }
 }
